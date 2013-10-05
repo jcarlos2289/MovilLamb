6fc14e3ad7fae9a5d15005955c18b021b6dc1086
Jose Carlos Rangel Ortiz <jockrlos2289@gmail.com>
2013-10-05 14:34:27 -0500

    Final

---
+++ DesktopApp/wawawa/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: LaiFISC
+ * Date: 04/10/2013
+ * Time: 12:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace wawawa
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnConectar = new System.Windows.Forms.Button();
+			this.CBoxMensajes = new System.Windows.Forms.ComboBox();
+			this.TBoxModem_Info = new System.Windows.Forms.Label();
+			this.CBoxComList = new System.Windows.Forms.ComboBox();
+			this.lblmensajesMemoria = new System.Windows.Forms.Label();
+			this.rtbMensajeMostrado = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnConectar
+			// 
+			this.btnConectar.Location = new System.Drawing.Point(12, 12);
+			this.btnConectar.Name = "btnConectar";
+			this.btnConectar.Size = new System.Drawing.Size(75, 23);
+			this.btnConectar.TabIndex = 0;
+			this.btnConectar.Text = "Conectar";
+			this.btnConectar.UseVisualStyleBackColor = true;
+			this.btnConectar.Click += new System.EventHandler(this.BtnConectarClick);
+			// 
+			// CBoxMensajes
+			// 
+			this.CBoxMensajes.FormattingEnabled = true;
+			this.CBoxMensajes.Location = new System.Drawing.Point(56, 54);
+			this.CBoxMensajes.Name = "CBoxMensajes";
+			this.CBoxMensajes.Size = new System.Drawing.Size(85, 21);
+			this.CBoxMensajes.TabIndex = 1;
+			this.CBoxMensajes.SelectedIndexChanged += new System.EventHandler(this.CBoxMensajesSelectedIndexChanged);
+			// 
+			// TBoxModem_Info
+			// 
+			this.TBoxModem_Info.Location = new System.Drawing.Point(266, 12);
+			this.TBoxModem_Info.Name = "TBoxModem_Info";
+			this.TBoxModem_Info.Size = new System.Drawing.Size(152, 51);
+			this.TBoxModem_Info.TabIndex = 2;
+			this.TBoxModem_Info.Text = "label1";
+			// 
+			// CBoxComList
+			// 
+			this.CBoxComList.FormattingEnabled = true;
+			this.CBoxComList.Location = new System.Drawing.Point(170, 12);
+			this.CBoxComList.Name = "CBoxComList";
+			this.CBoxComList.Size = new System.Drawing.Size(77, 21);
+			this.CBoxComList.TabIndex = 3;
+			// 
+			// lblmensajesMemoria
+			// 
+			this.lblmensajesMemoria.Location = new System.Drawing.Point(147, 52);
+			this.lblmensajesMemoria.Name = "lblmensajesMemoria";
+			this.lblmensajesMemoria.Size = new System.Drawing.Size(176, 23);
+			this.lblmensajesMemoria.TabIndex = 4;
+			this.lblmensajesMemoria.Text = "label1";
+			// 
+			// rtbMensajeMostrado
+			// 
+			this.rtbMensajeMostrado.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.rtbMensajeMostrado.Location = new System.Drawing.Point(12, 117);
+			this.rtbMensajeMostrado.Name = "rtbMensajeMostrado";
+			this.rtbMensajeMostrado.Size = new System.Drawing.Size(332, 118);
+			this.rtbMensajeMostrado.TabIndex = 6;
+			this.rtbMensajeMostrado.Text = "";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(100, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(70, 19);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Puerto COM:";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 58);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 19);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "# SMS:";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(439, 261);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.rtbMensajeMostrado);
+			this.Controls.Add(this.lblmensajesMemoria);
+			this.Controls.Add(this.CBoxComList);
+			this.Controls.Add(this.TBoxModem_Info);
+			this.Controls.Add(this.CBoxMensajes);
+			this.Controls.Add(this.btnConectar);
+			this.Name = "MainForm";
+			this.Text = "SMS APP";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox rtbMensajeMostrado;
+		private System.Windows.Forms.Label lblmensajesMemoria;
+		private System.Windows.Forms.ComboBox CBoxComList;
+		private System.Windows.Forms.Label TBoxModem_Info;
+		private System.Windows.Forms.ComboBox CBoxMensajes;
+		private System.Windows.Forms.Button btnConectar;
+	}
+}
---
+++ DesktopApp/wawawa/MainForm.cs
@@ -0,0 +1,291 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: LaiFISC
+ * Date: 04/10/2013
+ * Time: 12:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+using MySql.Data.MySqlClient;
+
+namespace wawawa
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public mCore.SMS objSMS = new mCore.SMS();
+        public mCore.Inbox objINBOX;
+        MySqlConnection con = new MySqlConnection();
+        MySqlDataReader rdr = null;
+        
+       // ConeccionDBServer CServer = new ConeccionDBServer();
+        
+		public MainForm()
+		{
+			InitializeComponent();			
+		}
+			
+		public void Conectar()
+         {
+             try
+             {
+                 String conecion;
+                 conecion = "Server=localhost;Port=3306;Database=movilamb;Uid=root;Pwd=;";
+
+                 con.ConnectionString = conecion;
+                 con.Open(); 
+                 MessageBox.Show("Conectado a la Base de Datos Server");
+                 con.Close();
+                 //lol();
+
+             }
+             catch (MySqlException q)
+             {
+             	MessageBox.Show(q.Message + q.ToString(), "Error al Cargar la Base de Datos" );
+             }
+         }
+		
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			Conectar();
+			if (objSMS.IsConnected == false)
+                {
+                    CBoxComList.Items.Clear();
+                    string[] ports = SerialPort.GetPortNames();
+                    foreach (string port in ports)
+                    {
+                        CBoxComList.Items.Add(port);
+                    }
+                    if (CBoxComList.Items.Count != 0)
+                    {
+                        CBoxComList.SelectedIndex = 0;
+                        //Con_Des();
+                        objINBOX = objSMS.Inbox();
+                        ModemConnect();
+                    }
+                    else
+                    {
+                        MessageBox.Show("THERE IS NO MODEM CONNECTED");
+                    }
+                }
+                if (objSMS.IsConnected == true)
+                {
+                    //Auto();
+                }
+		}
+		
+		void BtnConectarClick(object sender, EventArgs e)
+		{			
+			//DeshabilitarBotones();
+            if (objSMS.IsConnected == false)
+            {
+                ModemConnect();
+            }
+            //Con_Des();
+		}
+		
+		public void ModemConnect()
+        {
+            try
+            {
+                TBoxModem_Info.Text = "Connectando ...";
+                objSMS.Port = "" + CBoxComList.SelectedItem;
+                objSMS.BaudRate = mCore.BaudRate.BaudRate_9600;
+                objSMS.DataBits = mCore.DataBits.Eight;
+                objSMS.StopBits = mCore.StopBits.One;
+
+//                if (txtPIN.Text != "" )
+//                    objSMS.PIN = txtPIN.Text;
+
+                objSMS.Parity = mCore.Parity.None;
+                objSMS.FlowControl = mCore.FlowControl.RTS_CTS;
+                objSMS.Connect();
+                //------------------------------------------------------------
+                objINBOX.Concatenate = true;
+                objSMS.MessageMemory = mCore.MessageMemory.SM;
+                objINBOX.Refresh();
+
+                //txtPIN.Visible = false;
+                
+                if (objSMS.IsConnected == true)
+                {
+                    TBoxModem_Info.Text = ">>>>Moden Info<<<<" + "\n";
+                    TBoxModem_Info.Text += " Conectado al Puerto COM: " + CBoxComList.SelectedItem;
+                    //btnInfo_modem.Enabled = true;
+                    Mensajes();
+                }
+                else
+                {
+                 //   MessageBox.Show("There was an issue - Could not connect to port " + CBoxComList.SelectedItem + "\nConnection problem with the port" + CBoxComList.SelectedItem);
+                }
+            }
+            catch (mCore.GeneralException ex)
+            {
+//                if (modo == 1)
+//                {
+//                    Autoconnect();
+//                }
+//                else
+//                {
+//                    MessageBox.Show(ex.Message + "\n\n-Connection Problem-");
+//                    TBoxModem_Info.Text = "Connection Problem \nPlease try connecting the Modem";
+//                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }	
+		
+		public void Mensajes()
+        {
+            objINBOX.Refresh();
+            CBoxMensajes.Items.Clear();
+            for (int i = 1; i <= objINBOX.Count; i++)
+            {
+                CBoxMensajes.Items.Add(i);
+            }
+            lblmensajesMemoria.Text = "Messages in Memory " + CBoxMensajes.Items.Count;
+            if (objINBOX.HasMessages)
+            {
+            	int m = Convert.ToInt32(CBoxMensajes.Text);
+				
+				DividirMensaje(objINBOX.Message(m).Text + " " + objSMS.Inbox().Message(m).Phone);
+                rtbMensajeMostrado.Text = objINBOX.Message(Convert.ToInt32(CBoxMensajes.Text)).Text;
+            }
+            
+            else if (objSMS.Inbox().IsEmpty)
+            {
+                objINBOX.Refresh();
+                Actualizar();
+            }
+        }
+		
+		public void Actualizar() 
+        {
+            if (objINBOX.IsEmpty)
+            {
+                Mensajes();
+            }
+        }
+			
+		public void Eliminar_Mensaje()
+        {
+			if(CBoxMensajes.Text != ""){
+           int i = Convert.ToInt32(CBoxMensajes.Text);
+           objSMS.Inbox().Message(i).Delete();
+           Mensajes();
+			}
+        }
+		
+		void CBoxMensajesSelectedIndexChanged(object sender, EventArgs e)
+		{			
+			if (CBoxMensajes.Text != "")
+			{
+				int m = Convert.ToInt32(CBoxMensajes.Text);
+				DividirMensaje(objINBOX.Message(m).Text + " " + objSMS.Inbox().Message(m).Phone);
+				
+			}
+		}
+		
+		string nombre ="";
+		Int32 codigo =0;
+		string latitud ="";
+		string longitud ="";
+		string remitente ="";
+		
+		public void DividirMensaje(string Dato)
+        {
+			try{
+            if (Dato == "0\r")
+            { }
+            else
+            {
+                string mensajito = Dato, msg = "";
+
+                char[] delimit = new char[] { ' ' };
+                int i = 1;
+                foreach (string mensajillo in mensajito.Split(delimit))
+                {
+                    if (i == 1)
+                    {
+                        msg = mensajillo + " ";
+                        i++;
+                    }
+                    else
+                    {
+                        if (i == 2)
+                        {         
+                        	nombre = msg + mensajillo;
+                            msg = "Nombre del Paciente: " + nombre + "\n";
+                        	i++;                          
+                        }
+                        else
+                    	{
+	                        if (i == 3)
+	                        {          
+	                        	codigo = Convert.ToInt32(mensajillo);
+	                            msg += "Codigo: " + codigo  + "\n";
+	                        	i++;                          
+	                        }
+	                        else
+	                    	{
+		                        if (i == 4)
+		                        {            
+		                        	latitud = mensajillo;
+		                            msg += "Latitud: " + latitud  + "\n";
+		                        	i++;                          
+		                        }
+	                        	else
+		                    	{
+			                        if (i == 5)
+			                        {            
+			                        	longitud = mensajillo;
+			                            msg += "Longitud: " + longitud  + "\n";	
+										i++;   			                            
+			                        }
+			                        else 
+			                    	{
+				                        if (i == 6)
+				                        {            
+				                        	remitente = mensajillo;
+				                            msg += "Remitente: " + remitente;	                        
+				                        }
+			                        
+			                    	}
+		                    	}
+	                    	}
+                    	}
+                    }
+                }                
+            	rtbMensajeMostrado.Text = msg;
+            	lol();
+            }}
+			catch(Exception er)
+			{
+				rtbMensajeMostrado.Text = "Mensaje Erroneo";				
+            Eliminar_Mensaje();    
+			}
+        }
+		 public void lol() 
+         {		 	
+		 	con.Open();
+		 	MySqlCommand command = new MySqlCommand("INSERT INTO recibido(codigo, Nombre, Latitud, Longitud, Numero) VALUES (" + "1,'" + nombre + "', '" +   latitud + "'" + ", '" + longitud + "', '" + remitente + "'" + ")", con);
+            command.ExecuteNonQuery();
+            con.Close();
+            Eliminar_Mensaje();
+		 	
+		}
+				
+	}
+}
+		
+		
